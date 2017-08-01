b7a298e926bed3cc2430eab3122986970931d72d
Gabzer <gab.zer.nas@gmail.com>
2017-08-01 15:05:02 -0300

    Interncionalização funcionando, falta HTML no txt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { TranslateService } from 'ng2-translate';
+import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import { OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+
+  private subscription: Subscription;
+  
+  constructor(private translate: TranslateService, private activatedRoute: ActivatedRoute){
+    translate.addLangs(["en", "pt", "fr"]);
+    translate.setDefaultLang("en");
+
+    let browserlang = translate.getBrowserLang();
+    translate.use(browserlang.match(/en|pt|fr/) ? browserlang : "en");
+    //translate.use("pt");
+  }
+
+  changeLanguage(lang){
+    this.translate.use(lang);
+  }
+
+  ngOnInit(){
+    this.subscription = this.activatedRoute.queryParams.subscribe(
+      (param: any) => {
+        let locale = param['locale'];
+        if(locale != undefined){
+          this.translate.use(locale)
+        }
+      }
+    )
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
 }
